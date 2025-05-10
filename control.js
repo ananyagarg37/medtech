0e893b1d14e38531ec5626c8f15c334f1522d95f
Ananya <ananyagarg1666@gmail.com>
2025-05-10 19:39:08 +0530

    Update control.js

--- control.js
+++ control.js
@@ -1,2 +1,4 @@
 document.getElementById('waitingListForm').addEventListener('submit', async (e) => {
     e.preventDefault();
+   // Get form inputs
+    const name = document.getElementById('name').value.trim();
